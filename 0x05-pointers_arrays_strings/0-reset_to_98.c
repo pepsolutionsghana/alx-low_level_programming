08ba63da7b453dee865218daae6c174830e395a7
pepsolutionsghana <pepsolutionsghana@gmail.com>
2022-10-24 10:16:23 -0700

    Task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * Void reset_to_98 -take a pointer and update it to 98
- * @n : a pointer
- * Return : Void, Basically nothin is returned
- */
-
-void reset_to98(int *n)// reset_to_98
-{ *n = 98; }
+#include "main.h"	
+	/**
+	* reset_to_98 - ur comment
+	* @n: pointer
+	* Return: void
+	*/
+	void reset_to_98(int*n)
+	{   *n = 98;
+		}
