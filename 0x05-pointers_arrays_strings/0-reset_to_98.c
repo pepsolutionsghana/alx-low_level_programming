f2b7c02277d2b44c3fdf06df7874503373bdd3e7
pepsolutionsghana <pepsolutionsghana@gmail.com>
2022-10-25 02:28:31 -0700

    Task4

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,5 +5,5 @@
 	* Return: void
 	*/
 	void reset_to_98(int*n)
-	{   *n = 98;
-		}
+	{ *n = 98
+		};
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,12 @@
 #include "main.h"	
-	#include <string.h>
+#include <string.h>
 	
 
-	/**
-	* print_rev - string in reverse
-	* @s: string
-	*/
-	void print_rev(char *s)
+/**
+* print_rev - string in reverse
+* @s: string
+*/
+void print_rev(char *s)
 	{
 		int i, len;
 	
@@ -14,8 +14,6 @@
 		len = strlen(s);
 		for (i = len - 1; i >= 0; i--)
 			_putchar(s[i]);
-	
-
 		_putchar('\n');
 	}
 
