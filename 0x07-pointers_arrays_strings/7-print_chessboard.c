fa71d867a05ea336ee7a62455040b695913eab87
pepsolutionsghana <pepsolutionsghana@gmail.com>
2022-11-02 03:03:52 -0700

    Task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,20 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_chessboard - prints 2D array of chessboard
- * @a: chessboard array
- * Return: void
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
  */
-
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-	int row, col;
+	int i, n, total1 = 0, total2 = 0;
 
-	for (row = 0; row < 8; row++)
-	{
-		for (col = 0; col < 8; col++)
-		{
-			_putchar(a[row][col]);
-		}
-		_putchar('\n');
-	}
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		total1 = total1 + a[i];
+
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+		total2 = total2 + a[n];
+	printf("%d, %d\n", total1, total2);
 }
